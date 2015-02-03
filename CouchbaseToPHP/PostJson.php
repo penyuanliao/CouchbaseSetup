7f6b01a9242cc70d70eaf812a6c354d9020668d2
penyuanliao <anaylst@gmail.com>
2015-02-03 16:44:33 +0800

    測試

--- CouchbaseToPHP/PostJson.php
+++ CouchbaseToPHP/PostJson.php
@@ -2,30 +2,40 @@
 	//檢查是不是POST
 	if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	{
+		header("responseURL: text1111");
 		//送出去的header為json
 		header('Content-Type: application/json; charset=utf-8');
 		//接收post Content-Type:json; 
 		$json = file_get_contents('php://input');
 		$obj = json_decode($json);
 		
-		$cluster = new CouchbaseCluster(constant("127.0.0.1"));
+		$cluster = new CouchbaseCluster("127.0.0.1");
 		
+		$db = $cluster->openBucket("default");
+		
+		$loadavgstats = sys_getloadavg();
+		
+		$res = $db->insert($obj->key, array('id'=>$obj->id,'data'=>$obj->data,'cpu'=>$loadavgstats,'ram'=>getServerMemoryUsage()));
+		
+		$res = $db->get($obj->key);
+		
+		$doc = $res->value;
 
-		$posts[] = array("foo" => "bar","uname" => "oop!", "postValue"=> $obj[0]->uname);
+		// $posts[] = array("foo" => "bar","uname" => "oop!", "postValue"=> $obj[0]->uname);
 		
-		 echo json_encode($posts);
+		 echo json_encode($doc);
 	}
-	
-	// $data = (array) $_POST;
-	// $cluster = new CouchbaseCluster(constant("127.0.0.1"));
-	
-	// $this->db = $cluster->openBucket("default");
-	// $loadavgstats = sys_getloadavg();
-	// $res = $db->insert($data["key"], array('id'=>$data["id"],'data'=>$data["data"],'cpu'=>$loadavgstats,'ram'=>getServerMemoryUsage()));
-	
-	// $res = $db->get($data["key"]);
-	// $doc = $res->value;
-	
-	// echo json_encode($doc);
-	
+
+function getServerMemoryUsage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+}
 ?>
--- CouchbaseToPHP/PostformData.php
+++ CouchbaseToPHP/PostformData.php
@@ -3,20 +3,38 @@
 	if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	{
 		header('Content-Type: application/json; charset=utf-8');
-		$posts[] = array("foo" => "bar","uname" => "oop!");
-		 echo json_encode($posts);
+		
+		$docName = $_POST["key"];
+		$id = $_POST["id"];
+		$data = $_POST["data"];
+		
+		$cluster = new CouchbaseCluster("127.0.0.1");
+		
+		$db = $cluster->openBucket("default");
+		
+		$loadavgstats = sys_getloadavg();
+		
+		$res = $db->insert($docName, array('id'=>$id,'data'=>$data,'cpu'=>$loadavgstats,'ram'=>getServerMemoryUsage()));
+		
+		$res = $db->get($docName);
+		
+		$doc = $res->value;
+		
+		echo json_encode($doc);
 	}
 	
-	$data = (array) $_POST;
-	$cluster = new CouchbaseCluster(constant("127.0.0.1"));
-	
-	$this->db = $cluster->openBucket("default");
-	$loadavgstats = sys_getloadavg();
-	$res = $db->insert($data["key"], array('id'=>$data["id"],'data'=>$data["data"],'cpu'=>$loadavgstats,'ram'=>getServerMemoryUsage()));
-	
-	$res = $db->get($data["key"]);
-	$doc = $res->value;
-	
-	echo json_encode($doc);
+function getServerMemoryUsage()
+{
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+}
 	
 ?>
